9899fc7466a9501c53e1c132fac9e82623fa12fa
Yann Le Saint <podologue.brindas@gmail.com>
2023-08-16 16:01:19 +0200

    exo 2 css de qualité douteuse

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,21 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   movies = Movie.create([{ name: 'Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'removing data'
+Rastaurant.destroy_all
+
+puts 'creating resto'
+
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'belgian' }
+dominos = { name: 'Dominos Pizza', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+verone = { name: 'Pizza Verone', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+craponnois = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'french' }
+sushi = { name: 'Ichi Sushi', address: '56A Shoreditch High St, London E1 6PQ', category: 'japanese' }
+
+[dishoom, dominos, verone, craponnois, sushi].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
